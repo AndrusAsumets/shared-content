9f3862984e6237c3133a19dc9763eafb0bd63b12
Andrus Asumets <andrus.asumets@gmail.com>
2017-08-23 11:34:30 +0000

    Updates

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
 $.get('manifest.json', function(data) {
-	console.log(data)
+	var manifest = JSON.parse(data)
+	
+	for (i = 0; i < manifest.length; i++) {
+		var fileName = manifest[i].fileName
+		
+		$.get(fileName, function(data) {
+			var file = JSON.parse(data)
+			
+			for (i = 0; i < file.length; i++) {
+				var item = file[i]
+				
+				console.log(item)
+			}
+		})
+	}
 })
