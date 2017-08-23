4438c851a2b083f5a06c76460f5888b081b3ec4f
Andrus Asumets <andrus.asumets@gmail.com>
2017-08-23 11:22:08 +0000

    Updates

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,6 +3,6 @@ var ghpages = require('gh-pages')
 ghpages.publish('./', 
 	function(err) {
 		if (err) console.log(err)
-		else console.log("Successfully published all the contents of shared-content to GitHub's gh-pages branch.")
+		else console.log("Successfully published all the content of shared-content to GitHub's gh-pages branch.")
 	}
 )
